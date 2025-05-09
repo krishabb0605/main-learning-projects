7ba12cd355422686ad76c612fb1c15f0feaf43b5
krisha bhingaradiya <krishabhingaradiya1234@gmail.com>
2025-05-09 14:34:15 +0530

    Navigation between different pages

--- nextjs-demo/src/app/about/page.tsx
+++ nextjs-demo/src/app/about/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 // route /about
+
+// OnClick `go home` router push home path and it navigate to home page.
 const About = () => {
+  const router = useRouter();
   return (
-    <div>About us.</div>
+    <div>
+      <h1>About us.</h1>
+      <button onClick={() => router.push("/")} className='bg-blue-500 text-white rounded-md'>
+        Go Home
+      </button>
+    </div>
   )
 }
 
--- nextjs-demo/src/app/layout.tsx
+++ nextjs-demo/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navigation from "./components/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <header className="bg-slate-900 text-white p-4 text-center">
-          <p>Welcome to next.js course</p>
+          {/* <p>Welcome to next.js course</p> */}
+          <Navigation />
         </header>
         {children}
         <footer className="bg-slate-900 text-white p-4 text-center">

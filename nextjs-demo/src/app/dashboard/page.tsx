aef36263a87324356269a6f5cf2cfab4c8fe84e2
krisha bhingaradiya <krishabhingaradiya1234@gmail.com>
2025-05-12 11:13:06 +0530

    File Colocation

---
+++ nextjs-demo/src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import LineChart from './line-chart'
+
+// In accessible function
+function BarChart() {
+    return <h1>Bar chart</h1>
+}
+
+export default function Dashboard() {
+    return (
+        <div>
+            <p>Dashboard</p>
+            <LineChart />
+        </div>
+    )
+}

0e9385fe3591d4bc25ba145c67f1c2c8f611f4d1
krisha bhingaradiya <krishabhingaradiya1234@gmail.com>
2025-05-12 14:24:39 +0530

    Routing Metadata

--- nextjs-demo/src/app/products/[id]/page.tsx
+++ nextjs-demo/src/app/products/[id]/page.tsx
@@ -1,9 +1,37 @@
+// "use client"
+// You can't use metadata in client component.
+
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import React from 'react'
 
+// In title show product page when we reach to  this route. 
+// otherwise it shows `create next app`
+
+// `metadata` and `generateMetadata` cannot be exported at the same time
+// export const metadata: Metadata = {
+//     title: "Product page.",
+// };
+
+type Props = {
+    params: Promise<{ id: string }>
+}
+
+// Using this it gives title as per id. strickly give name as `generateMetadata`
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const { id } = await params;
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`iphone ${id}`)
+        }, 2000);
+    })
+    return {
+        title: `Product ${title}`,
+    };
+};
 
 // route /products/:id
-const Product = async ({ params }: { params: { id: string } }) => {
+const Product = async ({ params }: Props) => {
     const { id } = await params;
 
     if (parseInt(id) > 1000) {
@@ -14,4 +42,8 @@ const Product = async ({ params }: { params: { id: string } }) => {
     )
 }
 
-export default Product
+export default Product;
+
+// If you want to use client component and also want to use  metadata then 
+// make 1 file with client component and
+// another file with metadata. and import it in your file that with metadata file.

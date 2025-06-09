a0826f551fbd520c456525b6358c179609c2190e
krisha bhingaradiya <krishabhingaradiya1234@gmail.com>
2025-06-09 10:02:25 +0530

    Seperating server actions

---
+++ nextjs-demo/src/actions/products.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { addProducts } from "@/prisma-db";
+import { redirect } from "next/navigation";
+
+export type Errors = {
+    title?: string;
+    price?: string;
+    description?: string;
+}
+
+export type FormState = {
+    error: Errors;
+}
+
+export async function createProduct(prevState: FormState, formData: FormData) {
+    const title = formData.get("title") as string;
+    const price = formData.get("price") as string;
+    const description = formData.get("description") as string;
+
+    const errors: Errors = {};
+
+    if (!title) {
+        errors.title = "Title is required";
+    }
+    if (!price) {
+        errors.price = "Price is required";
+    }
+    if (!description) {
+        errors.description = "Description is required";
+    }
+    if (Object.keys(errors).length > 0) {
+        return { errors };
+    }
+
+    await addProducts(title, parseInt(price), description);
+    redirect("/products-db");
+}

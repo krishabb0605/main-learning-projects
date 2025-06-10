85e10d8c73c7c13f9e53b864735d538330f92619
krisha bhingaradiya <krishabhingaradiya1234@gmail.com>
2025-06-10 10:53:07 +0530

    Role based access control

---
+++ authentication-demo/src/app/admin/actions.ts
@@ -0,0 +1,50 @@
+"use server"
+
+import { auth, clerkClient } from "@clerk/nextjs/server"
+import { Roles } from "../../../types/globals";
+import { revalidatePath } from "next/cache";
+
+export async function setRole(formData: FormData) {
+    const { sessionClaims } = await auth();
+
+    if (!(sessionClaims?.metadata?.role)?.includes('admin')) {
+        throw new Error('Not authorized...');
+    }
+
+    const client = await clerkClient();
+
+    const id = formData.get("id") as string;
+    const role = formData.get("role") as Roles;
+
+    try {
+        await client.users.updateUser(id, {
+            publicMetadata: { role }
+        });
+
+        revalidatePath("/admin");
+    } catch {
+        throw new Error('Failed to update role...');
+    }
+}
+
+export async function removeRole(formData: FormData) {
+    const { sessionClaims } = await auth();
+
+    if (!(sessionClaims?.metadata?.role)?.includes('admin')) {
+        throw new Error('Not authorized...');
+    }
+
+    const client = await clerkClient();
+
+    const id = formData.get("id") as string;
+
+    try {
+        await client.users.updateUser(id, {
+            publicMetadata: { role: null }
+        });
+
+        revalidatePath("/admin");
+    } catch {
+        throw new Error('Failed to remove role...');
+    }
+}
--- authentication-demo/src/middleware.ts
+++ authentication-demo/src/middleware.ts
@@ -1,14 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 // If user not signdin it redirect to signin page when navigating to /user-profile...
 // const isProtectedRoute = createRouteMatcher(["/user-profile"])
 
 // If user not signdin it redirect to signin page when navigating page excluding this 3 rooutes.
 const isPubliucRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"])
+const isAdminRoute = createRouteMatcher(["/admin(.*)"])
 
 export default clerkMiddleware(async (auth, req) => {
   // if (isProtectedRoute(req)) await auth.protect();
   // if (!isPubliucRoute(req)) await auth.protect();
+  const role = (await auth()).sessionClaims?.metadata?.role;
+
+ if (isAdminRoute(req) && (!role || !role.includes("admin"))) {
+    const url = new URL("/", req.url);
+    return NextResponse.redirect(url);
+  }
 
   const { userId, redirectToSignIn } = await auth();
 
